f3f17b63cbb71dab2e46bd5f5166b6fbc0f5811f
Akhil-A0402 <akhil.a.nair760@gmail.com>
2024-12-13 09:43:20 +0530

    Three Largest Distinct Elements: OnePhase soln

--- Arrays/Beginners/ThreeLargestDistinctElements.java
+++ Arrays/Beginners/ThreeLargestDistinctElements.java
@@ -38,10 +38,44 @@ public class ThreeLargestDistinctElements {
         
     }
 
+    public static int[] onePhaseApproach(int[] arr){
+        int largest = -1, secondLargest = -1, thirdLargest = -1;
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]>largest){
+                thirdLargest = secondLargest;
+                secondLargest = largest;
+                largest = arr[i];
+            }else if(arr[i]>secondLargest && arr[i]!=largest){
+                thirdLargest = secondLargest;
+                secondLargest = arr[i];
+            }else if(arr[i]>thirdLargest && arr[i]!=largest && arr[i]!=secondLargest){
+                thirdLargest = arr[i];
+            }
+        }
+
+        List<Integer> listElements = new ArrayList<Integer>();
+        
+        // int[] elements = new int[3];
+        if(largest!=-1){listElements.add(largest);}
+        
+        if(secondLargest!=-1){listElements.add(secondLargest);}
+        
+        if(thirdLargest!=-1){listElements.add(thirdLargest);}
+        
+        return listElements.stream().mapToInt(i -> i).toArray();
+        
+    }
+
     public static void main(String[] args) {
         int[] output = getThreeLargest(new int[]{10, 4, 3, 50, 23, 90, 90});
         for(int i=0;i<output.length;i++){
             System.out.print(output[i] + " ");
+        }
+        System.out.println();
+        System.out.println("----------------------------------------");
+        int[] output2 = onePhaseApproach(new int[]{10, 4, 3, 50, 23, 90, 90});
+        for(int i=0;i<output2.length;i++){
+            System.out.print(output2[i] + " ");
         }
     }
 }
