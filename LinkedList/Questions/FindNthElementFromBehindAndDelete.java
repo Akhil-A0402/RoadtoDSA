1a38fa6611d04823dfc2815440356e870599f291
Akhil-A0402 <akhil.a.nair760@gmail.com>
2025-03-10 08:11:06 +0530

    Find anr remove nth element from behind impl using LL collections

---
+++ LinkedList/Questions/FindNthElementFromBehindAndDelete.java
@@ -0,0 +1,31 @@
+package LinkedList.Questions;
+
+import java.util.LinkedList;
+
+public class FindNthElementFromBehindAndDelete {
+
+    public static LinkedList<String> removeElement(LinkedList<String> list, int n){
+        if(list.isEmpty()){
+            return list;
+        }
+
+        list.remove(list.size()-n);
+
+        return list;
+    }
+
+    public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Cherry");
+        list.add("Mango");
+        list.add("Orange");
+        list.add("Grapes");
+        System.out.println(list);
+        removeElement(list, 3);
+        System.out.println(list);
+        System.err.println("Testing");
+
+    }
+}
