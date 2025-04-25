02496b921646358ac3d3d68e26df5f7659a4545d
Akhil-A0402 <akhil.a.nair760@gmail.com>
2025-04-25 21:37:44 +0530

    bellman ford algo

--- Graph/Impl/ShortestPathAlgo/BellmanFordAlgorithm.java
+++ Graph/Impl/ShortestPathAlgo/BellmanFordAlgorithm.java
@@ -32,6 +32,27 @@ public class BellmanFordAlgorithm {
 
     }
 
+
+    public static void createGraphNeg(ArrayList<Edge> graph[]) {
+        for(int i = 0;i<graph.length;i++){
+            graph[i] = new ArrayList<Edge>();
+        }
+
+        graph[0].add(new Edge(0,1,2));
+        graph[0].add(new Edge(0,2,4));
+
+        graph[1].add(new Edge(1,2,-4));
+
+        graph[2].add(new Edge(2,3,2));
+
+        graph[3].add(new Edge(3,4,4));
+
+        graph[4].add(new Edge(4,1,-10));
+
+
+    }
+
+
     // Time complexity : O(V.E)
     // This doesn't work for graph with negative weight cycle
     public static void shortestPathAlgo(ArrayList<Edge> graph[], int src, int V) {
@@ -55,6 +76,22 @@ public class BellmanFordAlgorithm {
             }
         }
 
+
+        // Detect if negative cycle exists in graph
+        // Iterate the graph one more time 
+
+        for(int i=0;i<V;i++) {
+            for(int j=0;j<graph[i].size();j++) {
+                Edge e = graph[i].get(j);
+                int u = e.src;
+                int v = e.dest;
+
+                if(dist[u]!=Integer.MAX_VALUE && dist[u] + e.wt < dist[v] ){
+                    System.out.println("Negative weight cycle exists");
+                }
+            }
+        }
+
         for(int i=0;i<dist.length;i++) {
             System.out.print(dist[i] + ", ");
         }
@@ -66,6 +103,10 @@ public class BellmanFordAlgorithm {
         ArrayList<Edge> graph[] = new ArrayList[V];
 
         createGraph(graph);
+
+        shortestPathAlgo(graph, 0, V);
+
+        createGraphNeg(graph);
 
         shortestPathAlgo(graph, 0, V);
     }
